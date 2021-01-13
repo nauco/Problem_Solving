68faf969f93843102fbf5b79ced120718dbee915
HODONG <hodong@gmail.com>
2021-01-13 13:49:34 +0900

    change h-index

--- 프로그래머스/정렬/H-Index.py
+++ 프로그래머스/정렬/H-Index.py
@@ -1,12 +1,10 @@
-#더 좋은 풀이가 있음, 다시풀기
 def solution(citations):
-    answer = len(citations)
+    answer = 0
     citations.sort()
-    for i in reversed(range(len(citations))):
-        if(len(citations)-i == citations[i]):
-            return citations[i]
-        elif(len(citations)-i > citations[i]):
-            for j in reversed(range(0,len(citations)+1)):
-                if(len(citations)-i-1 == j):
-                    return j
+    for idx, h in enumerate(reversed(citations)):
+        if idx < h:
+            answer += 1
+        else:
+            break
+        
     return answer
